d17ee86bf554666b0037135a0504867e65932a13
Bruno Coutinho <brunodba@live.com>
2020-05-12 19:19:56 -0300

    Login de acesso.

--- vendor/brunodba/php-classes/src/Model/User.php
+++ vendor/brunodba/php-classes/src/Model/User.php
@@ -9,6 +9,8 @@ use \Hcode\Model;
 class User extends Model {
     // Criamos uma constante para futuramente possibilitar a reutilização.
     const SESSION = "User";
+    const SECRET = "HcodePhp7_Secret";
+
     public static function login($login, $password){
         $sql = new Sql();
         $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
@@ -52,5 +54,90 @@ class User extends Model {
     public static function logout()
     {
         $_SESSION[User::SESSION] = null;
+    }
+
+    public static function listAll()
+    {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+    }
+
+    public function save()
+    {
+        $sql = new Sql();
+        $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            "desperson"=>$this->getdesperson(),
+            "deslogin"=>$this->getdeslogin(),
+            "despassword"=>$this->getdespassword(),
+            "desemail"=>$this->getdesemail(),
+            "nrphone"=>$this->getnrphone(),
+            "inadmin"=>$this->getinadmin()
+        ));
+        $this->setData($results[0]);
+    }
+
+    public function get($iduser)
+    {
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+            ":iduser"=>$iduser
+        ));
+        $this->setData($results[0]);
+    }
+
+    public function update()
+    {
+        $sql = new Sql();
+        $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            "iduser"=>$this->getiduser(),
+            "desperson"=>$this->getdesperson(),
+            "deslogin"=>$this->getdeslogin(),
+            "despassword"=>$this->getdespassword(),
+            "desemail"=>$this->getdesemail(),
+            "nrphone"=>$this->getnrphone(),
+            "inadmin"=>$this->getinadmin()
+        ));
+        $this->setData($results[0]);
+    }
+
+    public function delete()
+    {
+        $sql = new Sql();
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser"=>$this->getiduser()
+        ));
+    }
+
+    public static function getForgot($email)
+    {
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;", array(
+            ":email"=>$email
+        ));
+        if (count($results) === 0)
+        {
+            throw new \Exception("Não foi possível recuperar a senha.");
+        }
+        else
+        {
+            $data = $results[0];
+            $recoveries = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+                ":iduser"=>$data["iduser"],
+                ":desip"=>$_SERVER["REMOTE_ADDR"]
+            ));
+            if (count($recoveries) === 0)
+            {
+                throw new \Exception("Não foi possível recuperar a senha");
+            }
+            else
+            {
+                $dataRecovery = $recoveries[0];
+                $cipher="AES-128-ECB";
+                $ivlen = openssl_cipher_iv_length($cipher);
+                $iv = openssl_random_pseudo_bytes($ivlen);
+                $code = base64_encode(openssl_encrypt($dataRecovery["idrecovery"],$cipher,User::SECRET,$options = 0, $iv));
+                $link = "http://www.bcoecommerce.com.br/admin/forgot/reset?code=$code";
+            }
+        }
     }
 }
